86531fc4780ee5bb2dc05b2fab3cb9627b1172a2
RAJAT <rjt9058@gmail.com>
2022-08-25 11:46:27 +0530

    Update conn.js
    bhgj

--- server/db/conn.js
+++ server/db/conn.js
@@ -3,6 +3,6 @@ const mongoose= require('mongoose');
 const DB = process.env.DATABASE;
 
 mongoose.connect(DB).then(()=>{
-    console.log('connection successfull')
+    console.log('mongo connection successfull')
     console.log('server is running on 5000')
 }).catch((err)=> console.log('no connection'))
